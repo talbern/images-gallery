361dc65bea081418ef7bad93b2d6931e69b73b78
Tal Bernard <tal.bernard2@gmail.com>
2023-01-28 16:49:09 -0500

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#eeeeee',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
-      <Container className="justify-content-center">
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
-      </Container>
+      <Container className="justify-content-center"></Container>
+      <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
     </Navbar>
   );
 };
