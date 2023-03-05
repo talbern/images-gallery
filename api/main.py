c173e688c825a5b3d258399d7968db947bbb8079
Tal Bernard <tal.bernard2@gmail.com>
2023-03-04 19:04:13 -0500

    flask server setup

--- api/main.py
+++ api/main.py
@@ -1,11 +1,25 @@
-from flask import Flask
+import requests
+from flask import Flask, request
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY="ER7s-ax3ucCu4Ubi_4S1zOcFJNIiyLnmVkdOFUBWJtY"
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    
+    headers = {
+      "Accept-Version": "v1",
+      "Authorization": "Client-ID %s" % UNSPLASH_KEY
+    }
+    params = {
+      "query": word
+    }
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5050)
